f4690f89a5102327b0d8cccb4dc8bc09564792c5
mewadayogesh <145269954+mewadayogesh@users.noreply.github.com>
2026-07-29 15:37:58 +0530

    Update app.py

--- Project_user/app.py
+++ Project_user/app.py
@@ -17,9 +17,7 @@ from flask import (
 from openpyxl import Workbook
 import db
 
-MAIL_SENDER_EMAIL=digitalcrest781@gmail.com
-MAIL_SENDER_PASSWORD=your-16-char-app-password
-MAIL_RECIPIENT_EMAIL=ymewada6262@gmail.com
+
 
 app = Flask(__name__, static_folder='static', instance_relative_config=True)
 
@@ -47,9 +45,9 @@ db.init_app(app)
 # committed to source control or shared in plaintext anywhere, treat it as
 # compromised and rotate/regenerate it in your Google Account security
 # settings regardless of whether this app is public.
-MAIL_SENDER_EMAIL = os.environ.get('MAIL_SENDER_EMAIL', '')
-MAIL_SENDER_PASSWORD = os.environ.get('MAIL_SENDER_PASSWORD', '')
-MAIL_RECIPIENT_EMAIL = os.environ.get('MAIL_RECIPIENT_EMAIL', '')
+MAIL_SENDER_EMAIL = os.environ.get('digitalcrest781@gmail.com', '')
+MAIL_SENDER_PASSWORD = os.environ.get('Crest@2005#dnd@2', '')
+MAIL_RECIPIENT_EMAIL = os.environ.get('ymewada6262@gmail.com', '')
 
 
 # ---------------------------------------------------------------------------
